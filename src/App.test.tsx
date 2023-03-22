8fba5c66fc396432327d82a28d91184810909533
abel254 <abelmuema353@gmail.com>
2023-03-22 17:17:41 +0300

    fixed tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ import '@testing-library/jest-dom'
 
 test('button should be rendered', () => {
   render(<ChangeIcon />);
-  const buttonElement = screen.getByRole("btn");
+  const buttonElement = screen.getByRole("button");
   expect(buttonElement).toBeInTheDocument();
 });
